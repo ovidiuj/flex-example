67b990514d2be4750f08ed17a9941a7c40d12477
Ovidiu Juncanariu <Ovidiu.Juncanariu@mytoys.de>
2018-09-21 17:14:29 +0200

    create database schema

---
+++ src/Entity/City.php
@@ -0,0 +1,95 @@
+<?php
+namespace src\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Class City
+ * @package src\Entity
+ * @Entity @Table(name="cities")
+ */
+class City
+{
+    /**
+     * @Id @GeneratedValue @Column(type="integer")
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    private $cityName;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    private $timeZone;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    private $countryCode;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCityName()
+    {
+        return $this->cityName;
+    }
+
+    /**
+     * @param string $cityName
+     */
+    public function setCityName($cityName)
+    {
+        $this->cityName = $cityName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTimeZone()
+    {
+        return $this->timeZone;
+    }
+
+    /**
+     * @param string $timeZone
+     */
+    public function setTimeZone($timeZone)
+    {
+        $this->timeZone = $timeZone;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountryCode()
+    {
+        return $this->countryCode;
+    }
+
+    /**
+     * @param string $countryCode
+     */
+    public function setCountryCode($countryCode)
+    {
+        $this->countryCode = $countryCode;
+    }
+
+
+
+}
